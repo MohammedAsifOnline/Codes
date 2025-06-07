d51c386507a71486b17ca9c2f99b8b87e72e2043
MohammedAsifOnline <mohammedasifonline@gmail.com>
2025-06-07 14:48:21 +0530

    Add files via upload

---
+++ Name Game.py
@@ -0,0 +1,34 @@
+Ask_Name = input()
+
+if Ask_Name == 'aaiza':
+  print ('Brilliant')
+
+elif Ask_Name == 'aaiza':
+  print ('Brilliant')
+
+elif Ask_Name.lower() == 'ainaaz':
+  print ('Brilliant')
+
+elif Ask_Name.lower() == 'soha':
+  print ('Their Sister')
+
+elif Ask_Name.lower() == 'asif':
+  print ('Their Father')
+
+elif Ask_Name.lower() == 'nuzhat':
+  print ('Their Mother')
+
+elif Ask_Name.lower() == 'ajas':
+  print ('Their Uncle')
+
+elif Ask_Name.lower() == 'amina':
+  print ('Their Grand Mother')
+
+elif Ask_Name.lower() == 'ahmedi':
+  print ('Their Granny')
+
+elif Ask_Name.lower() == 'Asif':
+  print ('Their Father')
+
+else:
+  print ('Arrey ! I Dont Know')
